140838ca7b92b26a5445efdf3870902fbb25c292
Mike Vickers <mvickers@s3gov.com>
2021-07-16 13:04:32 -0500

    Added validation, prepped command

--- KentuckyWindage/KentuckyWindageForms/KentuckyWindageForms/Models/InputModel.cs
+++ KentuckyWindage/KentuckyWindageForms/KentuckyWindageForms/Models/InputModel.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace KentuckyWindageForms.Models
 {
     public class InputModel
     {
+        [Range(0.0d, double.PositiveInfinity, ErrorMessage = "The field {0} must be greater than {1}.")] 
         public decimal TargetSizeInches { get; set; }
+        [Range(0.0d, double.PositiveInfinity, ErrorMessage = "The field {0} must be greater than {1}.")]
         public decimal TargetSizeMilDots { get; set; }
-        public WindDirection WindDirection { get; set; }
+        [Range(0.0d, double.PositiveInfinity, ErrorMessage = "The field {0} must be greater than {1}.")]
         public decimal WindageInches { get; set; }
+        [Range(0.0d, double.PositiveInfinity, ErrorMessage = "The field {0} must be greater than {1}.")]
         public decimal ElevationInches { get; set; }
+        public WindDirection WindDirection { get; set; }
+
     }
 }
